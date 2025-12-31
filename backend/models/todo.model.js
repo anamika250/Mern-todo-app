88bc3ddb0ac312ee7ab281e9fbb0a8c63ddd989c
Anamika <anamika2004abc@gmail.com>
2025-12-31 19:33:29 +0530

    Initial Commit

---
+++ backend/models/todo.model.js
@@ -0,0 +1,19 @@
+import { text } from "express";
+import mongoose from "mongoose";
+
+const todoSchema = mongoose.Schema({
+    text: {
+        type: String,
+        required: true, 
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+}, {
+    timestamps: true,
+})
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+export default Todo;
