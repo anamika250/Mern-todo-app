9206c73b2b5a99da9ce9eb2bebed8ea045213950
Anamika <anamika2004abc@gmail.com>
2026-01-07 14:03:38 +0530

    Deploy frontend and backend on same server

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,39 @@
 import express from "express";
 import dotenv from "dotenv";
-import todoRoutes from "./routes/todo.route.js";
-import { connectDB } from "./config/db.js";
-import authRoutes from "./routes/auth.route.js";
 import cors from "cors";
 import path from "path";
+import todoRoutes from "./routes/todo.route.js";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+// Middlewares
 app.use(cors());
+app.use(express.json());
 
-app.use("/api/todos", todoRoutes);
+// API routes
 app.use("/api/auth", authRoutes);
+app.use("/api/todos", todoRoutes);
 
+// --------- SERVE FRONTEND (PRODUCTION) ----------
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get(/.*/, (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-});
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "frontend", "dist", "index.html")
+    );
+  });
 }
 
+// Start server
 app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log(`Server running on port ${PORT}`);
 });
